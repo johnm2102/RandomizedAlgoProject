5c6680f54e456b8c9feccc272cfd39e0841e13c8
johnm2102 <tln507@alumni.ku.dk>
2022-06-01 13:43:55 +0200

    added mulShift

---
+++ hashFunction.cs
@@ -0,0 +1,36 @@
+using System;
+/*
+This goal of this program is to implement multiply shift and 
+multiply-mod-hashing
+*/
+namespace RandomizedAlgo{
+    public class HashFuncs{
+        /*
+        Implementing a multiply shift hashing which is parameterized of "a" 
+        and "l", where "a" is a 64 bit integer, and 
+        where "l" is positive number less than 64.
+        More precisely, the hashfunction needs to be:
+            h(x) = (a*x)>>(64-l),
+            where a is an uneven 64 bit number 
+        */
+        public func<ulong, ulong> mulShift(ulong a, int l){
+            return new func<ulong, ulong>(x => ((a * x) >> (64 - l)));
+        }
+
+        /*
+        Implementing multiply-mod-prime hashing where p = 2^{89} - 1, and 
+        is parameterized as a, b, l, where a and b are numbers less than
+        p and where l is a positive number less than 64. More precisely:
+            h(x) = ((a*x + b) mod p) mod 2^l
+        where a and b are uniform
+        */
+        
+
+
+
+
+
+
+        
+    }
+}
